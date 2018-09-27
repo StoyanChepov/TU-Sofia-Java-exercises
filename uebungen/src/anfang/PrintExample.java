efa1f4387495996390856ef6c2d1b583c6df9ffc
201218028 <201218028@STUDWS208-16.FDIBA.local>
2018-09-27 08:18:58 +0300

    This is my first fdiba project created on 27.09.18

--- uebungen/src/anfang/PrintExample.java
+++ uebungen/src/anfang/PrintExample.java
@@ -1,5 +1,17 @@
 package anfang;
 
 public class PrintExample {
-
+	public static void main(String[] args) {
+		System.out.println("My name is Alan Turing.");
+		System.out.println("I was born in 1912.");
+		System.out.print("I am the father of");
+		System.out.print("theoretical computer science");
+		System.out.println("and artificial inteligence.");
+		System.out.print("I am also known for: \n");
+		System.out.print("Turing machine\n");
+		System.out.print("- Turing test \n");
+	
+		System.out.println();
+		System.out.print("My name is Alan Turing.\nI was born in 1912. \nI am the father of\ntheoretical computer science\nand artificial inteligence.I am also known for: \nTuring machine\n- Turing test \n");
+	}
 }
--- uebungen/src/anfang/Weihnachtsbaum.java
+++ uebungen/src/anfang/Weihnachtsbaum.java
@@ -1,5 +1,12 @@
 package anfang;
 
 public class Weihnachtsbaum {
-
+	public static void main (String[]args) {
+		System.out.println("    *    ");
+		System.out.println("   ***   ");
+		System.out.println("  *****  ");
+		System.out.println(" ******* ");
+		System.out.println("*********");
+		System.out.println("   ***   ");
+		}
 }
